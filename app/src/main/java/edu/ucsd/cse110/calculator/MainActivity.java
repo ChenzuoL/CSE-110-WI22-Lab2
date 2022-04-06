8cd22a8bfe7bc089f83dcc364683482aa67f27d3
chenz <chenzuoliu@gmail.com>
2022-04-06 16:27:35 -0700

    My first comment

--- app/src/main/java/edu/ucsd/cse110/calculator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/calculator/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         // There is a bug in this function! One of your exercises is to find it!
 
-        // Hello world this is a new commen!
-
+        // Hello world this is a new comment123!
+    
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
